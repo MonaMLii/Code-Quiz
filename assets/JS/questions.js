eea4bf1fba093bfea698e5af817a043858413a11
chunkybooboo <89316044+chunkybooboo@users.noreply.github.com>
2023-01-24 22:36:08 -0800

    commit all files

---
+++ assets/JS/questions.js
@@ -0,0 +1,117 @@
+// list of questions
+// question from https://www.includehelp.com/mcq/javascript-multiple-choice-questions-mcqs.aspx
+
+var questions = [
+    {
+        title: "Which is the correct JavaScript statement to display 'Hello World!' into an alert box?",
+        choices: [
+            "alert('Hello World!');", 
+            "alert(Hello World!);", 
+            "alert(Text:'Hello World!');", 
+            "Both A. and B."],
+        answer: 0
+    },
+    {
+        title: "Which type of JavaScript language is _____?",
+        choices: [
+            "Object-oriented",
+            "Object-based",
+            "Functional programming",
+            "All of the above"],
+        answer: 0
+    },
+    {
+        title: "Which of the following statement(s) is true about the JavaScript?",
+        choices: [
+            "It is a scripting language used to make the website interactive",
+            "It is an advanced version of Java for Desktop and Mobile application development",
+            "It is a markup language of Java to develop the webpages",
+            "All of the above"
+        ],
+        answer: 0
+    },
+    {
+        title: "In which HTML element, we put the JavaScript code?",
+        choices: [
+            "<javascript>...</javascript>",
+            "<js>...</js>",
+            "<script>...</script>",
+            "<css>...</css>"
+        ],
+        answer: 2
+    },
+    {
+        title: "Which JavaScript method is used to access an HTML element by id?",
+        choices: [
+            "getElementById()",
+            "getElement(id)",
+            "getElementById",
+            "elementById(id)"
+        ],
+        answer: 0
+    },
+    {
+        title: "Which JavaScript method is used to write on browser's console?",
+        choices: [
+            "console.write()",
+            "console.output()",
+            "console.log()",
+            "console.writeHTML()"
+        ],
+        answer: 2
+    },
+    {
+        title: "In JavaScript, single line comment begins with ___.",
+        choices: [
+            "#",
+            "/*",
+            "$",
+            "//"
+        ],
+        answer: 3
+    },
+    {
+        title: "What is the default value of an uninitialized variable?",
+        choices: [
+            "0",
+            "undefined",
+            "null",
+            "NaN"
+        ],
+        answer: 1
+    },
+    {
+        title: "JavaScript arrays are written with _____.",
+        choices: [
+            "round brackets ()",
+            "curly brackets {}",
+            "double quotes",
+            "square brackets []"
+        ],
+        answer: 3
+    },
+    {
+        title: "Which keyword is used to define a JavaScript function?",
+        choices: [
+            "module",
+            "fun",
+            "func",
+            "function"
+        ],
+        answer: 3
+    },
+    {
+        title: "Which property is used to get the length of a string in JavaScript?",
+        choices: [
+            "strlen",
+            "len",
+            "length",
+            "Length"
+        ],
+        answer: 2
+    }
+]
+
+
+var person 
+//
---
+++ assets/JS/script.js
@@ -0,0 +1,194 @@
+
+// create all vars
+
+var questionsEl = document.querySelector("#questions");
+var thisQuestion = document.querySelector("#question-is");
+var choicesEl = document.querySelector("#choices");
+var goodFeedbackEl = document.querySelector("#good-feedback");
+var badFeedbackEl = document.querySelector("#bad-feedback");
+var resultEl = document.querySelector("#result");
+var timerEl = document.querySelector("#time");
+var btnEl = document.querySelector("#buttons");
+var startBtn = document.querySelector("#start");
+var instructionEl = document.querySelector("#instruction");
+var submitBtn = document.querySelector("#submit");
+var initialEl = document.querySelector("#initial");
+var highScoreEl = document.querySelector("#high-score");
+var currentScore = 0;
+
+console.log(resultEl);
+
+// quiz time
+var currentQuestion = 0;
+var time = questions.length * 15;
+var timerStart;
+
+
+
+// popup box from Codepen + adjust by myself
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("instruction");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// // popup box for high scorefrom Codepen + adjust by myself
+
+// Get the modal
+var modal = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn = document.getElementById("high-score");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close2")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+// start timer onclick start
+startBtn.addEventListener("click", function() {
+    timerStart = setInterval(clockTick, 1000);
+    timerEl.textContent = time;
+    questionsEl.classList.remove("hide");
+    startBtn.classList.add("hide");
+    instructionEl.classList.add("hide");
+    renderQuestion();
+});
+
+
+
+function clockTick() {
+    // update time
+    time--;
+    timerEl.textContent = time;
+  
+    // check if user ran out of time
+    if (time <= 0) {
+      endQuiz();
+    }
+  }
+
+// render question (work with tutor)
+
+function renderQuestion () {
+   
+      var getQuestion = questions[currentQuestion];
+
+      var titleEl = document.getElementById("question-is");
+      titleEl.textContent = getQuestion.title;
+     
+      for (var i = 0; i < getQuestion.choices.length; i++) {
+        var currentBtn = document.getElementById("choice-" + i);
+        currentBtn.textContent = getQuestion.choices[i]
+      }
+      
+}
+
+//check answer 
+  
+function checkAnswer (index) {
+  var getQuestion = questions[currentQuestion];
+
+  var rightOption = index === getQuestion.answer;
+  if ( rightOption) {
+    currentScore ++;
+
+    goodFeedbackEl.classList.remove("hide");
+    setTimeout(() => {
+      goodFeedbackEl.classList.add("hide");
+    }, 500);
+
+  } else {
+    time -= 15;
+
+    badFeedbackEl.classList.remove("hide");
+    setTimeout(() => {
+      badFeedbackEl.classList.add("hide");
+    }, 500);
+  }
+
+  currentQuestion ++;
+
+  if (currentQuestion >= questions.length) {
+    endQuiz();
+  } else {
+    renderQuestion();
+  }
+}
+
+//******QUESTION********
+
+// end quiz
+function endQuiz(){
+  
+  timerEl.textContent = '';
+  clearInterval(timerStart);
+
+  questionsEl.classList.add("hide"); 
+
+  lastPage();
+
+}
+
+function lastPage() {
+  resultEl.classList.remove("hide");
+  var endScore = document.getElementById("end-score")
+  console.log(endScore);
+  endScore.textContent = currentScore;
+}
+
+function getHighScore() {
+  var initial = initialEl.value;
+
+  if (initial === "") {
+    return;
+  } 
+  var viewScore 
+}
+
+submitBtn.addEventListener("click", getHighScore);
+
+
+// store score
+// function storeScore(score) {
+//   localStorage.setItem("myScore", score)
+// }
+
+// storeScore("helo world")
